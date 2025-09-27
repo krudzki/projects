6622ee7c2327be6037871e75c72dd3826c38f99c
Krzysztof Rudzki <krzysztof.r.rudzki@gmail.com>
2025-09-27 19:49:42 +0200

    Project

---
+++ backend/models/Project.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const projectSchema = new mongoose.Schema({
+    name:        { type: String, required: true },
+    description: String,
+    owner:       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    tasks:       [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Project', projectSchema);
